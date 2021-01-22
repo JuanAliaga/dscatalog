23dd20b48b2e89f902294f45712fb273a718e47d
unknown <juan.felipe.aliaga@gmail.com>
2021-01-22 15:19:30 -0300

    Product repository, GET ok

---
+++ backend/src/main/java/com/jadev/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.jadev.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jadev.dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long>{
+
+}
