4214d88237c6104f06735fc382184d41da1f5f88
unknown <juan.felipe.aliaga@gmail.com>
2021-01-28 11:05:42 -0300

    Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/Navbar';
 
 
 const App = () =>{
     
     return(
-    <h1>Ola</h1>
-    
+    <Navbar/>
+
     );
 }
 
