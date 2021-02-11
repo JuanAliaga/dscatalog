a8e4af3da33e5ac88f7351ef373259ba542a59e5
unknown <juan.felipe.aliaga@gmail.com>
2021-02-11 17:16:45 -0300

    Users crud

---
+++ backend/src/main/java/com/jadev/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.jadev.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.jadev.dscatalog.entities.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long>{
+
+}
