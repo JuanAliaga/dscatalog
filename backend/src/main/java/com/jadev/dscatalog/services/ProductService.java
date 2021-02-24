642c4f293188915b4a336489653133ab834d624d
unknown <juan.felipe.aliaga@gmail.com>
2021-02-24 11:24:03 -0300

    Postgress compatibility

--- backend/src/main/java/com/jadev/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/jadev/dscatalog/services/ProductService.java
@@ -1,5 +1,6 @@
 package com.jadev.dscatalog.services;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -32,8 +33,9 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true) //garante a transação com o bd
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-		Page<Product> list = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Long categoryId,String name, PageRequest pageRequest){
+		List<Category> categories = (categoryId == 0) ? null: Arrays.asList(categoryRepository.getOne(categoryId));
+		Page<Product> list = repository.find(categories,name,pageRequest);
 		return list.map(x -> new ProductDTO(x));
 	}
 
