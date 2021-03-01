aa78dd36caaa981032cc1132ba7ea4dbc9992913
unknown <juan.felipe.aliaga@gmail.com>
2021-03-01 14:52:12 -0300

    Redirect failed authentication

--- front-web/src/core/utils/request.ts
+++ front-web/src/core/utils/request.ts
@@ -1,7 +1,8 @@
 import axios, { Method } from 'axios';
 import { type } from 'os';
 import qs from 'qs';
-import { CLIENT_ID, CLIENT_SECRET } from './auth';
+import { CLIENT_ID, CLIENT_SECRET, getSessionData } from './auth';
+import history from './history';
 
 type RequestParams ={
     method?: Method;
@@ -16,6 +17,16 @@ type LoginData = {
     password:string;
 }
 
+axios.interceptors.response.use(function(response){
+    return response;
+}, function(error){
+    if(error.response.status === 401){                                                                                                                                                                                                                  
+        console.log("redirecionar para a pagina de login");
+        history.push('/admin/auth/login');
+    }
+    return Promise.reject(error);
+});
+
 const BASE_URL = 'http://localhost:8080';
 export const makeRequest = ({method = 'GET', url, data,params,headers}: RequestParams) =>{
     return axios({
@@ -25,6 +36,15 @@ export const makeRequest = ({method = 'GET', url, data,params,headers}: RequestP
         params,
         headers
     });
+}
+
+export const makePrivateRequest = ({method = 'GET', url, data,params}: RequestParams) =>{
+    const sessionData = getSessionData();
+
+    const headers ={
+        'Authorization': `Bearer ${sessionData.access_token}`
+    }
+    return makeRequest({method,url,data,params,headers});
 }
 
 export const makeLogin = (loginData: LoginData) =>{
--- front-web/src/pages/Auth/components/Login/Index.tsx
+++ front-web/src/pages/Auth/components/Login/Index.tsx
@@ -1,10 +1,11 @@
 import ButtonIcon from 'core/components/buttonIcon';
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react';
+import {Link, useHistory} from 'react-router-dom'
 import {useForm} from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss'
 import { makeLogin } from 'core/utils/request';
+import { saveSessionData } from 'core/utils/auth';
 
 type FormData ={
     username: string;
@@ -12,17 +13,29 @@ type FormData ={
 }
 const Login = () =>{
     const {register, handleSubmit} = useForm();
+    const [hasError,setHasError] = useState(false);
+    const history = useHistory();
+
     const onSubmit = (data:FormData) => {
         console.log(data);
         makeLogin(data)
+        .then(response=>{
+            setHasError(false);
+            saveSessionData(response.data);
+            history.push('/admin');
+        })
+        .catch(() => {
+            setHasError(true);
+        })
     }
     return (
         <div>
             <AuthCard title="Login">
+                {hasError && (<div className="alert alert-danger">Usuario ou senha invalidos</div>)}
                 <form className="LoginForm" onSubmit={handleSubmit(onSubmit)}>
                    
-                    <input className="form-control input-base margin-bottom-30" type="email" placeholder="Email" name="username" ref={register} ></input>
-                    <input className="form-control input-base" type="password" placeholder="Password" name="password" ref={register}></input>
+                    <input className="form-control input-base margin-bottom-30" type="email" placeholder="Email" name="username" ref={register({required:true})} ></input>
+                    <input className="form-control input-base" type="password" placeholder="Password" name="password" ref={register({required:true})}></input>
                     
 
                     <Link to="/admin/auth/recover" className="login-link-recover">
